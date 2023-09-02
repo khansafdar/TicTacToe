99ee76d69c3c647365ca1c0222493a5b9d6723c7
safdarkhan <khandimpal01@gmail.com>
2023-09-02 23:15:46 +0530

    Functinality working fine for online player

--- app/src/main/java/com/example/myapplication/ui/view/OnlineGamepage.kt
+++ app/src/main/java/com/example/myapplication/ui/view/OnlineGamepage.kt
@@ -14,33 +14,24 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import java.util.Arrays
+import kotlin.system.exitProcess
 
-class OnlineGamepage:AppCompatActivity() {
+class OnlineGamepage : AppCompatActivity() {
     val view = arrayListOf<View>()
     var isMyMove = MutableLiveData(isCodeMaker)
     private lateinit var binding: OnlineGamePageBinding
     var gameActive = true
-
-    // Player representation
-    // 0 - X
-    // 1 - O
     private var activePlayer = 0
     var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
-
-    // State meanings:
-    //    0 - X
-    //    1 - O
-    //    2 - Null
-    // put all win positions in a 2D array
     var winPositions = arrayOf(
-            intArrayOf(0, 1, 2),
-            intArrayOf(3, 4, 5),
-            intArrayOf(6, 7, 8),
-            intArrayOf(0, 3, 6),
-            intArrayOf(1, 4, 7),
-            intArrayOf(2, 5, 8),
-            intArrayOf(0, 4, 8),
-            intArrayOf(2, 4, 6)
+        intArrayOf(0, 1, 2),
+        intArrayOf(3, 4, 5),
+        intArrayOf(6, 7, 8),
+        intArrayOf(0, 3, 6),
+        intArrayOf(1, 4, 7),
+        intArrayOf(2, 5, 8),
+        intArrayOf(0, 4, 8),
+        intArrayOf(2, 4, 6)
     )
     var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,41 +42,39 @@ class OnlineGamepage:AppCompatActivity() {
         handleOnClickListener()
         hideActionBar()
         gameReset()
-        if(isCodeMaker){
-            activePlayer=0
-        }else{
-            activePlayer=1
-        }
-        FirebaseDatabase.getInstance().reference.child("data").child(code).addChildEventListener(object:
-                ChildEventListener{
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                var data = snapshot.value
-                if(isMyMove.value==true){
-                    isMyMove.value = false
-                }else{
-                    isMyMove.value = true
-                    playerTap(view[data.toString().toInt()])
+        FirebaseDatabase.getInstance().reference.child("data").child(code)
+            .addChildEventListener(object :
+                ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    var data = snapshot.value
+                    if (isMyMove.value == true) {
+                        isMyMove.value = false
+                    } else {
+                        isMyMove.value = true
+                        moveOnLine(view[data.toString().toInt()])
+                        checkWinner()
+                    }
                 }
-            }
 
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
-            }
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
 
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-                TODO("Not yet implemented")
-            }
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    gameReset()
+                }
 
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
-            }
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    TODO("Not yet implemented")
+                }
 
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
 
-        })
+            })
     }
+
     private fun addView() {
         view.add(binding.imageView0)
         view.add(binding.imageView1)
@@ -98,12 +87,28 @@ class OnlineGamepage:AppCompatActivity() {
         view.add(binding.imageView8)
     }
 
+    private fun moveOnLine(view: View) {
+        counter++
+        val img = view as ImageView
+        val tappedImage = img.tag.toString().toInt()
+        gameState[tappedImage] = activePlayer
+        activePlayer = if (activePlayer == 0) {
+            img.setImageResource(R.drawable.x)
+            1
+        } else {
+            img.setImageResource(R.drawable.o)
+            0
+        }
+    }
+
     private fun observer() {
-        isMyMove.observe(this){
-            if(isMyMove.value==true){
-                binding.status.text = "Your turn"
-            }else{
-                binding.status.text = "Other turn"
+        isMyMove.observe(this) {
+            if(gameActive) {
+                if (isMyMove.value == true) {
+                    binding.status.text = "Your turn"
+                } else {
+                    binding.status.text = "Other turn"
+                }
             }
         }
     }
@@ -114,164 +119,129 @@ class OnlineGamepage:AppCompatActivity() {
 
     private fun handleOnClickListener() {
         binding.backButton.setOnClickListener {
-            finish()
+            removeCode()
+            FirebaseDatabase.getInstance().reference.child("data").child(code).removeValue()
+            exitProcess(0)
         }
-    }
-
-    private fun DisbleClick(){
-        binding.imageView0.isClickable  =  false
-        binding.imageView1.isClickable  =  false
-        binding.imageView2.isClickable  =  false
-        binding.imageView3.isClickable  =  false
-        binding.imageView4.isClickable  =  false
-        binding.imageView5.isClickable  =  false
-        binding.imageView6.isClickable  =  false
-        binding.imageView7.isClickable  =  false
-        binding.imageView8.isClickable  =  false
-    }
-    private fun EnableClick(){
-        binding.imageView0.isClickable  =  true
-        binding.imageView1.isClickable  =  true
-        binding.imageView2.isClickable  =  true
-        binding.imageView3.isClickable  =  true
-        binding.imageView4.isClickable  =  true
-        binding.imageView5.isClickable  =  true
-        binding.imageView6.isClickable  =  true
-        binding.imageView7.isClickable  =  true
-        binding.imageView8.isClickable  =  true
-    }
-    private fun StartButton(){
         binding.startButton.setOnClickListener {
-            EnableClick()
             gameReset()
+            FirebaseDatabase.getInstance().reference.child("data").child(code).removeValue()
+
         }
     }
 
-    private fun EnableTheClick() {
-        binding.imageView0.setOnClickListener{
-            binding.imageView0.isClickable = true
-        }
-        gameReset()
+    private fun DisbleClick() {
+        binding.imageView0.isClickable = false
+        binding.imageView1.isClickable = false
+        binding.imageView2.isClickable = false
+        binding.imageView3.isClickable = false
+        binding.imageView4.isClickable = false
+        binding.imageView5.isClickable = false
+        binding.imageView6.isClickable = false
+        binding.imageView7.isClickable = false
+        binding.imageView8.isClickable = false
     }
 
-
+    private fun EnableClick() {
+        binding.imageView0.isClickable = true
+        binding.imageView1.isClickable = true
+        binding.imageView2.isClickable = true
+        binding.imageView3.isClickable = true
+        binding.imageView4.isClickable = true
+        binding.imageView5.isClickable = true
+        binding.imageView6.isClickable = true
+        binding.imageView7.isClickable = true
+        binding.imageView8.isClickable = true
+    }
 
     fun playerTap(view: View) {
-        if(isMyMove.value==true) {
+        if (isMyMove.value == true) {
             binding.startButton.text = "Reset Game"
             val img = view as ImageView
             val tappedImage = img.tag.toString().toInt()
-
-            // game reset function will be called
-            // if someone wins or the boxes are full
-
-            // if the tapped image is empty
             if (gameState[tappedImage] === 2) {
-                // increase the counter
-                // after every tap
                 counter++
-
-                // check if its the last box
                 if (counter == 9) {
-                    // reset the game
                     gameActive = false
                 }
-
-                // mark this position
                 gameState[tappedImage] = activePlayer
-
-                // this will give a motion
-                // effect to the image
                 img.translationY = -1000f
-
-                // change the active player
-                // from 0 to 1 or 1 to 0
                 if (activePlayer == 0) {
-                    // set the image of x
                     img.setImageResource(R.drawable.x)
                     activePlayer = 1
-
-                    // change the status
                 } else {
-                    // set the image of o
                     img.setImageResource(R.drawable.o)
                     activePlayer = 0
-                    val status = findViewById<TextView>(R.id.status)
-
-                    // change the status
                 }
                 img.animate().translationYBy(1000f).duration = 300
                 updateDatabase(tappedImage)
             }
-            var flag = 0
-            // Check if any player has won if counter is > 4 as min 5 taps are
-            // required to declare a winner
-            if (counter > 4) {
-                for (winPosition in winPositions) {
-                    if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
-                        flag = 1
+            checkWinner()
+        }
+    }
 
-                        // Somebody has won! - Find out who!
-                        drawLine(winPosition)
-
-                        // game reset function be called
-                        gameActive = false
-                        val winnerStr: String = if (gameState[winPosition[0]] === 0) {
-                            DisbleClick()
-                            binding.startButton.text = "Reset Game"
-                            "You has won"
-                        } else {
-                            DisbleClick()
-                            binding.startButton.text = "Reset Game"
-                            "Other has won"
-                        }
-                        // Update the status bar for winner announcement
-                        binding.status.text = winnerStr
+    private fun checkWinner() {
+        var flag = 0
+        if (counter > 4) {
+            for (winPosition in winPositions) {
+                if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
+                    flag = 1
+                    drawLine(winPosition)
+                    gameActive = false
+                    val winnerStr: String = if (gameState[winPosition[0]] === 0) {
+                        DisbleClick()
+                        binding.startButton.text = "Reset Game"
+                        "You has won"
+                    } else {
+                        DisbleClick()
+                        binding.startButton.text = "Reset Game"
+                        "Other has won"
                     }
+                    binding.status.text = winnerStr
                 }
-                // set the status if the match draw
-                if (counter == 9 && flag == 0) {
-                    DisbleClick()
-                    binding.startButton.text = "Reset Game"
-                    binding.status.text = "Match Draw"
-                }
+            }
+            if (counter == 9 && flag == 0) {
+                DisbleClick()
+                binding.startButton.text = "Reset Game"
+                binding.status.text = "Match Draw"
             }
         }
     }
-    private fun updateDatabase(cellId:Int){
+
+    private fun updateDatabase(cellId: Int) {
         FirebaseDatabase.getInstance().reference.child("data").child(code).push().setValue(cellId);
     }
 
-    private fun drawLine(WinPosition:IntArray) {
-        if(WinPosition[0]==0 && WinPosition[1]==1 && WinPosition[2]==2){
+    private fun drawLine(WinPosition: IntArray) {
+        if (WinPosition[0] == 0 && WinPosition[1] == 1 && WinPosition[2] == 2) {
             binding.row1.apply {
                 setImageResource(R.drawable.horizontal)
             }
-        }else if(WinPosition[0]==3 && WinPosition[1]==4 && WinPosition[2]==5){
+        } else if (WinPosition[0] == 3 && WinPosition[1] == 4 && WinPosition[2] == 5) {
             binding.row2.apply {
                 setImageResource(R.drawable.horizontal)
             }
-        }else if(WinPosition[0]==6 && WinPosition[1]==7 && WinPosition[2]==8){
+        } else if (WinPosition[0] == 6 && WinPosition[1] == 7 && WinPosition[2] == 8) {
             binding.row3.apply {
                 setImageResource(R.drawable.horizontal)
             }
-        } else if(WinPosition[0]==0 && WinPosition[1]==3 && WinPosition[2]==6){
+        } else if (WinPosition[0] == 0 && WinPosition[1] == 3 && WinPosition[2] == 6) {
             binding.col1.apply {
                 setImageResource(R.drawable.veritcal)
             }
-        }else if(WinPosition[0]==1 && WinPosition[1]==4 && WinPosition[2]==7){
+        } else if (WinPosition[0] == 1 && WinPosition[1] == 4 && WinPosition[2] == 7) {
             binding.row2.apply {
                 setImageResource(R.drawable.veritcal)
             }
-        }else if(WinPosition[0]==2 && WinPosition[1]==5 && WinPosition[2]==8){
+        } else if (WinPosition[0] == 2 && WinPosition[1] == 5 && WinPosition[2] == 8) {
             binding.col3.apply {
                 setImageResource(R.drawable.veritcal)
             }
-        }else if(WinPosition[0]==0 && WinPosition[1]==4 && WinPosition[2]==8){
+        } else if (WinPosition[0] == 0 && WinPosition[1] == 4 && WinPosition[2] == 8) {
             binding.row2.apply {
                 setImageResource(R.drawable.maindiagonal)
             }
-        }else if(WinPosition[0]==2 && WinPosition[1]==4 && WinPosition[2]==6){
+        } else if (WinPosition[0] == 2 && WinPosition[1] == 4 && WinPosition[2] == 6) {
             binding.row2.apply {
                 setImageResource(R.drawable.offdiagonal)
             }
@@ -279,13 +249,12 @@ class OnlineGamepage:AppCompatActivity() {
     }
 
     private fun gameReset() {
+        isMyMove.value = isCodeMaker
+        activePlayer = if (isCodeMaker) {0} else {1}
         binding.startButton.text = "Click On Grid To play"
         gameActive = true
-        activePlayer = 0
         counter = 0
-        //set all position to Null
         Arrays.fill(gameState, 2)
-        // remove all the images from the boxes inside the grid
         binding.imageView0.setImageResource(0)
         binding.imageView1.setImageResource(0)
         binding.imageView2.setImageResource(0)
@@ -300,5 +269,17 @@ class OnlineGamepage:AppCompatActivity() {
         binding.row3.setImageResource(0)
         binding.col1.setImageResource(0)
         binding.col3.setImageResource(0)
+        EnableClick()
+    }
+    private fun removeCode(){
+        if(isCodeMaker){
+            FirebaseDatabase.getInstance().reference.child("codes").child(keyValue).removeValue()
+        }
+    }
+
+    override fun onBackPressed() {
+        removeCode()
+        FirebaseDatabase.getInstance().reference.child("data").child(code).removeValue()
+        exitProcess(0)
     }
 }
